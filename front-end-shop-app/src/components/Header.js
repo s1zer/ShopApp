4a9707cae330a76237f73bd02743da926f372214
s1zer <47991443+s1zer@users.noreply.github.com>
2020-01-10 22:08:18 +0100

    2020-08-01-Update

--- front-end-shop-app/src/components/Header.js
+++ front-end-shop-app/src/components/Header.js
@@ -6,7 +6,9 @@ class Header extends Component {
     state = {
         searchValue: "",
         searchType: ""
-    };
+    }
+
+    options = ["Option1", "Option2", "Option3", "Option4", "Option5"];
 
     searchPlaceholder = "Type to search something...";
 
@@ -23,6 +25,8 @@ class Header extends Component {
     }
 
     render() {
+
+        const options = this.options
         return (
             <Fragment>
                 <form className="searchForm">
@@ -31,11 +35,25 @@ class Header extends Component {
                                onChange={this.handleSearchValue}
                                type="text"/>
                         <select className="searchSelect">
-                            <option> asa</option>
+                            {options.map(o => <option key={o}>{o}</option>)}
                         </select>
                         <button className="searchButton">search</button>
                     </label>
+                    <nav className="navbar navbar-default navbarHeader">
+                        <div className="container-fluid">
+                            <div className="navbar-header">
+                                 <a className="navbar-brand" href="#">WebSiteName</a>
+                            </div>
+                            <ul className="nav navbar-nav">
+                                <li className="active"><a href="#">Home</a></li>
+                                <li><a href="#">Page 1</a></li>
+                                <li><a href="#">Page 2</a></li>
+                                <li><a href="#">Page 3</a></li>
+                            </ul>
+                        </div>
+                    </nav>
                 </form>
+
             </Fragment>
         )
     }
